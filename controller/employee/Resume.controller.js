168d9531d8709d383c352f3b01864253ac0e28d6
luciocabrera <lucio.cabrera@just-bi.nl>
2017-07-14 14:56:39 +0200

    step 9

--- controller/employee/Resume.controller.js
+++ controller/employee/Resume.controller.js
@@ -1,18 +1,25 @@
 sap.ui.define([
-    "sap/ui/demo/nav/controller/BaseController"
-], function(BaseController) {
+    "sap/ui/demo/nav/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
     "use strict";
+    var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 
     return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
+
         onInit: function() {
             var oRouter = this.getRouter();
+
+            this.getView().setModel(new JSONModel(), "view");
             oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
+
         },
         _onRouteMatched: function(oEvent) {
-            var oArgs, oView;
+            var oArgs, oView, oQuery;
 
             oArgs = oEvent.getParameter("arguments");
             oView = this.getView();
+
             oView.bindElement({
                 path: "/Employees(" + oArgs.employeeId + ")",
                 events: {
@@ -25,11 +32,34 @@ sap.ui.define([
                     }
                 }
             });
+
+            oQuery = oArgs["?Query"];
+
+            if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1) {
+                oView.getModel().setProperty("/selectedTabKey", oQuery.tab);
+            } else {
+                this.getRouter().navTo("employeeResume", {
+                    employeeId: oArgs.employeeId,
+                    query: {
+                        tab: _aValidTabKeys[0]
+                    }
+                }, true);
+            }
         },
         _onBindingChange: function(oEvent) {
             if (!this.getView().getBindingContext()) {
                 this.getRouter().getTargets().display("notFound");
             }
+        },
+        onTabSelect: function(oEvent) {
+
+            var oCtx = this.getView().getBindingContext();
+            this.getRouter().navTo("employeeResume", {
+                employeeId: oCtx.getProperty("EmployeeID"),
+                query: {
+                    tab: oEvent.getParameter("selectedKey")
+                }
+            }, true);
         }
     });
 });
