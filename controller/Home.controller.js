597b58324e376b8ea33d73686f492aaebe7efbff
luciocabrera <lucio.cabrera@just-bi.nl>
2017-07-17 14:40:38 +0200

    step 11 Navigate tutorial

--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -1,21 +1,25 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
-	"use strict";
+    "sap/ui/demo/nav/controller/BaseController"
+], function(BaseController) {
+    "use strict";
 
-	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
+    return BaseController.extend("sap.ui.demo.nav.controller.Home", {
 
-		onDisplayNotFound : function (oEvent) {
-			// display the "notFound" target without changing the hash
-			this.getRouter().getTargets().display("notFound", {
-				fromTarget : "home"
-			});
-		},
+        onDisplayNotFound: function(oEvent) {
+            // display the "notFound" target without changing the hash
+            this.getRouter().getTargets().display("notFound", {
+                fromTarget: "home"
+            });
+        },
 
-		onNavToEmployees : function (oEvent){
-			this.getRouter().navTo("employeeList");
-		}
+        onNavToEmployees: function(oEvent) {
+            this.getRouter().navTo("employeeList");
+        },
 
-	});
+        onNavToEmployeeOverview: function(oEvent) {
+            this.getRouter().navTo("employeeOverview");
+        }
+
+    });
 
 });
